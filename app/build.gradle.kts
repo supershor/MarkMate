3096013c7d9d3e50008451c77a2d7675dd012fa5
ASHISH SAHU <supershor.cp@gmail.com>
2025-12-08 19:29:34 +0530

    version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.supershor.markmate"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "10.0"
+        versionCode = 11
+        versionName = "11.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
